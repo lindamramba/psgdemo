01325e19a143d844209f4267a6785f0b48a68e0b
Linda Mramba <linda.mramba@ewx.co.za>
2022-11-16 15:38:28 +0200

    Moved hard coded API URL to a config file

--- SmartPhones.Business/SmartPhonesService.cs
+++ SmartPhones.Business/SmartPhonesService.cs
@@ -1,44 +1,55 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace SmartPhones.Business
 {
     public class SmartPhonesService : ISmartPhonesService
     {
+        private readonly IConfiguration _configuration;
+        public SmartPhonesService(IConfiguration configuration)
+        {
+            _configuration= configuration;
+        }
         public Product GetBestSmartPhone()
         {
-            RestClient client = new("https://dummyjson.com");
-            RestRequest request = new("products/category/smartphones", Method.Get);
+            var apiBaseUrl = _configuration.GetSection("SmartPhones:APIBaseUrl").Value;
 
-            var result = client.Execute(request);
-            if (result.IsSuccessful)
+            if(!string.IsNullOrWhiteSpace(apiBaseUrl))
             {
-                if (result.Content != null)
+                RestClient client = new(apiBaseUrl);
+                RestRequest request = new("products/category/smartphones", Method.Get);
+
+                var result = client.Execute(request);
+                if (result.IsSuccessful)
                 {
-                    var phoneData = JsonConvert.DeserializeObject<SmartPhone>(result.Content);
-
-                    if(phoneData != null)
+                    if (result.Content != null)
                     {
-                        if(phoneData.Products != null)
-                        {
-                            var bestSmartphone = phoneData.Products
-                            .Where(i => (i.Price - (i.Price * i.DiscountPercentage) / 100) < 500)
-                            .OrderBy(i => i.Rating)
-                            .LastOrDefault();
+                        var phoneData = JsonConvert.DeserializeObject<SmartPhone>(result.Content);
 
-                            if (bestSmartphone != null)
+                        if (phoneData != null)
+                        {
+                            if (phoneData.Products != null)
                             {
-                                return bestSmartphone;
-                            }
-                            else
-                            {
-                                return new Product();
+                                var bestSmartphone = phoneData.Products
+                                    .Where(i => (i.Price - (i.Price * i.DiscountPercentage) / 100) < 500)
+                                    .OrderBy(i => i.Rating)
+                                    .LastOrDefault();
+
+                                if (bestSmartphone != null)
+                                {
+                                    return bestSmartphone;
+                                }
+                                else
+                                {
+                                    return new Product();
+                                }
                             }
                         }
                     }
                 }
             }
-
+            
             return new Product();
         } 
     }
